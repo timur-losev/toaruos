700b3371da4fca215d5f4760ecd7f9c7579a7edd
Timur Losev <timur.losev@gmail.com>
2016-07-24 17:21:09 +0300

    - statrted doing build system

---
+++ build/main.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python3
+from __future__ import ( division, absolute_import, print_function, unicode_literals)
+
+if __name__ == "__main__":
+    import main
+    raise SystemExit(main.main())
+
+import os, sys, tempfile, logging
+import shutil
+from subprocess import call
+
+if sys.version_info >= (3,):
+    import urllib.request as urllib2
+    import urllib.parse as urlparse
+else:
+    import urllib2
+    import urlparse
+
+def downloadFile(url, saveTo):
+    u = urllib2.urlopen(url)
+
+    scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
+    filename = saveTo + os.path.basename(path)
+
+    with open(filename, 'wb') as f:
+        meta = u.info()
+        items = meta.items()
+        item = [item for item in items if item[0].lower() == 'Content-Length'.lower()]
+        headerName, metaLength = item[0]
+        fileSize = None
+        if metaLength:
+            fileSize = int(metaLength)
+        print("Downloading: {0} Bytes: {1}".format(url, fileSize))
+
+        fileSizeDL = 0
+        blockSize = 8192
+        while True:
+            buffer = u.read(blockSize)
+            if not  buffer:
+                break
+
+            fileSizeDL += len(buffer)
+            f.write(buffer)
+
+            status = "{0:16}".format(fileSizeDL)
+
+            if fileSize:
+                status += "   [{0:6.2f}%]".format(fileSizeDL * 100 / fileSize)
+            status += chr(13)
+            print(status, end="")
+        print()
+
+def grabTarbal(url, name, desc=None):
+    print("Pulling {0}".format(desc))
+
+    if os.path.exists("../toolchain/tarballs/" + name):
+        print("Exists")
+    else:
+        downloadFile(url + "/" + name, '../toolchain/tarballs/')
+
+def prepare():
+
+    grabTarbal("http://www.netgull.com/gcc/releases/gcc-4.6.4", "gcc-core-4.6.4.tar.gz", "gcc")
+    grabTarbal("http://www.netgull.com/gcc/releases/gcc-4.6.4", "gcc-g++-4.6.4.tar.gz", "g++")
+    grabTarbal("http://ftp.gnu.org/gnu/binutils", "binutils-2.22.tar.gz", "binutils")
+    grabTarbal("http://b.dakko.us/~klange/mirrors", "newlib-1.19.0.tar.gz", "newlib")
+    grabTarbal("http://download.savannah.gnu.org/releases/freetype", "freetype-2.4.9.tar.gz", "freetype")
+    grabTarbal("http://zlib.net", "zlib-1.2.8.tar.gz", "zlib")
+    grabTarbal("http://b.dakko.us/~klange/mirrors", "libpng-1.5.13.tar.gz", "libpng")
+    grabTarbal("http://www.cairographics.org/releases", "pixman-0.26.2.tar.gz", "pixman")
+    grabTarbal("http://www.cairographics.org/releases", "cairo-1.12.2.tar.xz", "cairo")
+    grabTarbal("http://b.dakko.us/~klange/mirrors", "MesaLib-7.5.2.tar.gz", "mesa")
+    grabTarbal("http://b.dakko.us/~klange/mirrors", "ncurses-5.9.tar.gz" , "ncurses")
+    grabTarbal("ftp://ftp.vim.org/pub/vim/unix", "vim-7.3.tar.bz2", "vim")
+
+def main():
+    workDir = os.getcwd()
+    print(workDir)
+
+    prepare()
+
+
