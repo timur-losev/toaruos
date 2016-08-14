e9ba3b675a79b04d55ce38a5cf3b08a05945471d
Timur Losev <timur.losev@gmail.com>
2016-08-14 22:48:59 +0300

    - fixed newlib file patching

--- build/main.py
+++ build/main.py
@@ -9,6 +9,7 @@ import os, sys, tempfile, logging
 import shutil
 import subprocess
 import tarfile
+import glob
 
 kTarbalsDir = "../toolchain/tarballs/"
 kPatchesDir = "../toolchain/patches/"
@@ -82,58 +83,80 @@ def unpackTarbal(tarbal):
 
     print("Extracted {0}".format(tarbal.pk))
 
-def prepare():
 
-    if not os.path.exists(kTarbalsDir):
-        os.mkdir(kTarbalsDir)
+tarbals = [
+    Tarbal("http://www.netgull.com/gcc/releases/gcc-4.6.4", "gcc-core-4.6.4.tar.gz", "gcc", "gcc-4.6.4"),
+    Tarbal("http://www.netgull.com/gcc/releases/gcc-4.6.4", "gcc-g++-4.6.4.tar.gz", "g++"),
+    Tarbal("http://ftp.gnu.org/gnu/binutils", "binutils-2.22.tar.gz", "binutils", "binutils-2.22"),
+    Tarbal("http://b.dakko.us/~klange/mirrors", "newlib-1.19.0.tar.gz", "newlib", "newlib-1.19.0"),
+    Tarbal("http://download.savannah.gnu.org/releases/freetype", "freetype-2.4.9.tar.gz", "freetype", "freetype-2.4.9"),
+    Tarbal("http://zlib.net", "zlib-1.2.8.tar.gz", "zlib"),
+    Tarbal("http://b.dakko.us/~klange/mirrors", "libpng-1.5.13.tar.gz", "libpng", "libpng-1.5.13"),
+    Tarbal("http://www.cairographics.org/releases", "pixman-0.26.2.tar.gz", "pixman", "pixman-0.26.2"),
+    Tarbal("http://www.cairographics.org/releases", "cairo-1.12.2.tar.xz", "cairo", "cairo-1.12.2"),
+    Tarbal("http://b.dakko.us/~klange/mirrors", "MesaLib-7.5.2.tar.gz", "mesa", "Mesa-7.5.2"),
+    Tarbal("http://b.dakko.us/~klange/mirrors", "ncurses-5.9.tar.gz", "ncurses", "ncurses-5.9"),
+    Tarbal("ftp://ftp.vim.org/pub/vim/unix", "vim-7.3.tar.bz2", "vim", "vim73")
+]
 
-    tarbals = [
-        Tarbal("http://www.netgull.com/gcc/releases/gcc-4.6.4", "gcc-core-4.6.4.tar.gz", "gcc", "gcc-4.6.4"),
-        Tarbal("http://www.netgull.com/gcc/releases/gcc-4.6.4", "gcc-g++-4.6.4.tar.gz", "g++"),
-        Tarbal("http://ftp.gnu.org/gnu/binutils", "binutils-2.22.tar.gz", "binutils", "binutils-2.22"),
-        Tarbal("http://b.dakko.us/~klange/mirrors", "newlib-1.19.0.tar.gz", "newlib", "newlib-1.19.0"),
-        Tarbal("http://download.savannah.gnu.org/releases/freetype", "freetype-2.4.9.tar.gz", "freetype", "freetype-2.4.9"),
-        Tarbal("http://zlib.net", "zlib-1.2.8.tar.gz", "zlib"),
-        Tarbal("http://b.dakko.us/~klange/mirrors", "libpng-1.5.13.tar.gz", "libpng", "libpng-1.5.13"),
-        Tarbal("http://www.cairographics.org/releases", "pixman-0.26.2.tar.gz", "pixman", "pixman-0.26.2"),
-        Tarbal("http://www.cairographics.org/releases", "cairo-1.12.2.tar.xz", "cairo", "cairo-1.12.2"),
-        Tarbal("http://b.dakko.us/~klange/mirrors", "MesaLib-7.5.2.tar.gz", "mesa", "Mesa-7.5.2"),
-        Tarbal("http://b.dakko.us/~klange/mirrors", "ncurses-5.9.tar.gz", "ncurses", "ncurses-5.9"),
-        Tarbal("ftp://ftp.vim.org/pub/vim/unix", "vim-7.3.tar.bz2", "vim", "vim73")
-    ]
 
-# Download dependencies
+def download_dependencies():
     for tb in tarbals:
         grabTarbal(tb)
 
-# Remove old depencencies tree
+
+def remove_old_dependencies_tree():
     upackedTarballs = os.listdir(kTarbalsDir)
     for dir in upackedTarballs:
         dir = os.path.abspath(kTarbalsDir + dir)
         if os.path.isdir(dir):
             shutil.rmtree(dir)
 
-# Unpack newest tarbals
+
+def unpack_tarbals():
     for tb in tarbals:
         unpackTarbal(tb)
 
-# Apply needed patches
+def apply_patches():
     for tb in tarbals:
         if tb.patchItem:
             command = "patch -p1 < " + "../../patches/" + tb.patchItem + ".patch"
             subprocess.Popen(command, shell=True, cwd=kTarbalsDir+tb.patchItem)
 
-# Install newlib staff
+
+def install_newlib_stuff():
     newlibPatchesDir = kPatchesDir + "newlib/"
-    newlibFolder = kTarbalsDir + "newlib-1.19.0/newlib/libc/";
+    newlibFolder = kTarbalsDir + "newlib-1.19.0/newlib/libc/"
     shutil.copytree(newlibPatchesDir + "toaru", newlibFolder + "sys/toaru")
-    shutil.copytree(newlibPatchesDir + "include", newlibFolder + "sys/toaru/include")
+
+    for filename in glob.glob(os.path.join(newlibPatchesDir + 'include', '*.*')):
+        shutil.copy(filename, newlibFolder + "sys/toaru")
 
     shutil.copy(newlibPatchesDir + "setjmp.S", newlibFolder + "machine/i386/")
 
     shutil.copy(newlibPatchesDir + "wcwidth.c", newlibFolder + "string/")
     shutil.copy(newlibPatchesDir + "wcswidth.c", newlibFolder + "string/")
 
+
+def prepare():
+
+    if not os.path.exists(kTarbalsDir):
+        os.mkdir(kTarbalsDir)
+
+# Download dependencies
+    download_dependencies()
+
+# Remove old depencencies tree
+    remove_old_dependencies_tree()
+
+# Unpack newest tarbals
+    unpack_tarbals()
+
+# Apply needed patches
+    apply_patches()
+
+# Install newlib staff
+    install_newlib_stuff()
 
 
 def main():
